dd8ffda21005f1992d0a2176f1edaadf90619032
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-07-10 22:16:27 +0200

    feat(listAllPublicRepositories): adding the function of listing all public repos.

--- src/listAllPublicRepositories.py
+++ src/listAllPublicRepositories.py
@@ -1,16 +1,15 @@
 """
-Function: getAllRepositories
+Function: listAllPublicRepositories
 Method: GET
-Utility: Get all repositories of a user provided.
+Utility: List all public repositories of a user
 """
 
 import requests
 
+GITHUB_API_LINK = "https://api.github.com"
 
-GITHUB_API_LINK = "https://api.github.com/"
 
-
-def getAllRepositories(user):
+def listAllPublicRepositories(user):
     url = f"{GITHUB_API_LINK}/users/{user}/repos"
     response = requests.get(url)
     if response.status_code == 200:

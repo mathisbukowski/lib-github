cd19a8d9beaf0eef004592577bc1590abcb2b2a7
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-07-12 01:57:36 +0200

    chore(pullrequests): remove a part of code who is duplicated

--- src/Classes/pullRequestClass.py
+++ src/Classes/pullRequestClass.py
@@ -10,17 +10,6 @@ import requests
 GITHUB_API_LINK = "https://api.github.com"
 
 
-def getPullRequestByNumber(owner, repo, pull_number):
-    url = f"{GITHUB_API_LINK}/repos/{owner}/{repo}/pulls/{pull_number}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        content = response.json()
-        return content
-    else:
-        print("Error: Invalid Data Provided.")
-        return None
-
-
 class PullRequest:
     def __init__(self, token, repo):
         self.repo_name = repo

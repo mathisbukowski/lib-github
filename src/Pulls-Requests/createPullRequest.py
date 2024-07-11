57bd0ef9752994bf536db9611eadba74629adfa6
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-07-11 10:29:57 +0200

    chore(createPullRequest): editing the return of the function

--- src/Pulls-Requests/createPullRequest.py
+++ src/Pulls-Requests/createPullRequest.py
@@ -3,6 +3,7 @@ Function: createPullRequest
 Method: POST
 Utility: create a pull request
 """
+import json
 
 import requests
 
@@ -14,8 +15,7 @@ def createPullRequest(owner, repo, title, body, base, branchToBeMerged):
     request = {'title': title, 'body': body, 'head': branchToBeMerged, 'base': base}
     response = requests.post(url, params=request)
     if response.status_code == 201:
-        content = response.json()
-        return content
+        return json.dumps(response)
     else:
         print("Error: Invalid Data Provided.")
         return []

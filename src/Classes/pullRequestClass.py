3974b679a0a402800ff31ef95257a5bbb703a991
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-07-12 12:39:17 +0200

    feat(pullrequests): add the mergePullRequest method

--- src/Classes/pullRequestClass.py
+++ src/Classes/pullRequestClass.py
@@ -56,3 +56,13 @@ class PullRequest:
         else:
             print(f"Error: {response.status_code} - {response.json().get('message', 'Invalid Data Provided.')}")
             return response.json()
+
+    def mergePullRequest(self, pull_number, commit_title, commit_message, sha, merge_method):
+        url = f"{GITHUB_API_LINK}/repos/{self.repo_name}/pulls/{pull_number}/merge"
+        request = {'commit_title': commit_title, 'commit_message': commit_message, 'sha': sha, 'merge_method': merge_method}
+        response = requests.put(url, headers=self.headers, json=request)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: {response.status_code} - {response.json().get('message', 'Invalid Data Provided.')}")
+            return response.json()
